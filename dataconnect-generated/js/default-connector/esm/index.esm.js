3cc309fabecda6a82603bde3a495ea928be05521
Hetul Mistry <Manishcmdgm812@gmail.com>
2025-08-07 17:11:49 +0530

    feat: ui and bsaic implementation
    Still full of errors

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'assignmate',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'assignmate',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
