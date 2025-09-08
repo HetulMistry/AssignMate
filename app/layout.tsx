3ed00e7afc9659dc5e671be268963bb2f7899181
Hetul Mistry <manishcmdgm812@gmail.com>
2025-09-08 17:12:08 +0530

    Add: Favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="./favicon.svg" type="image/svg+xml" />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
